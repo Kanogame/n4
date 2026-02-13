5206b709c4a943b234ac5b4823fc59ddd4dfd50a
kanogame <wasdqert1222@gmail.com>
2026-02-13 12:07:02 +0700

    func added

---
+++ autograd/function.py
@@ -0,0 +1,14 @@
+
+class function():
+    """
+    Default interface for any function that supports 
+    forward and backward passes
+    """
+
+    def forward_pass():
+        """forward pass of function"""
+        pass
+
+    def backward_pass():
+        """backward pass of function"""
+        pass
