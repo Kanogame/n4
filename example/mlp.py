961f2326da954f27ffee13cb6674d6b7a08f5441
kanogame <wasdqert1222@gmail.com>
2026-04-10 12:00:20 +0700

    mlp moved (issue with import atm)

--- example/mlp.py
+++ example/mlp.py
@@ -1,21 +1,21 @@
+from typing import Self
+import random
+
 from n4.tensor import Tensor
 from n4.op import Relu, NonOp
-from n4.core import Value
-from typing import Self
 from n4.numeric import PyFloat
 from n4.nn import Model, DenseLayer, SoftmaxLayer, Sequential
-from n4.nn.loss import CrossEntropyLoss
-from n4.optim import SGD, Adam
-
-import random
+from n4.loss import CrossEntropyLoss
+from n4.optim import SGD
 
 
 class MyModel(Model[PyFloat]):
     def __init__(self: Self) -> None:
         self.backend = PyFloat
         self.model = Sequential(
-            DenseLayer(1, 2, self.backend, Relu),
-            DenseLayer(2, 3, self.backend, NonOp),
+            DenseLayer(1, 12, self.backend, Relu),
+            DenseLayer(12, 12, self.backend, Relu),
+            DenseLayer(12, 3, self.backend, NonOp),
             SoftmaxLayer(self.backend),
         )
 
@@ -45,9 +45,6 @@ for i in range(200):
     loss.backward()
     opt.step()
     print(f"loss: {loss.data}")
-
-# graph = loss.collect_graph()
-# graph.to_graphviz("out")
 
 
 print(preds[0][0].data, preds[0][1].data, preds[0][2].data)
--- src/n4/loss/__init__.py
+++ src/n4/loss/__init__.py
@@ -1,3 +1,5 @@
 from .loss import Loss
+from .CrossEntropyLoss import CrossEntropyLoss
+from .MSELoss import MSELoss
 
-__all__ = ["Loss"]
+__all__ = ["Loss", "CrossEntropyLoss", "MSELoss"]
